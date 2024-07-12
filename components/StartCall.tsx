391928cf879ea7d4726e13ceb2897edbf1395d4b
devsrijit <srijitghosh7331@gmail.com>
2024-07-12 18:30:04 +0530

    refactor: Update StartCall component with EVI description

--- components/StartCall.tsx
+++ components/StartCall.tsx
@@ -28,15 +28,16 @@ export default function StartCall() {
               backgroundColor="black"
               rangeY={800}
               particleCount={500}
-              
               className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-full"
             >
               <h2 className="text-white text-2xl md:text-6xl font-bold text-center">
-                The hell is this?
+                The hell is EVI ?
               </h2>
               <p className="text-white text-sm md:text-2xl max-w-xl mt-6 text-center">
-                This is chemical burn. It&apos;ll hurt more than you&apos;ve
-                ever been burned and you&apos;ll have a scar.
+                EVI stands for Empathetic Voice Interface.
+                In a world full of noise, EVI is the quiet voice that listens.
+                He talks to you like a real friend, navigating through the
+                emotions of human life.
               </p>
               <Button
                 className={"z-50 flex items-center gap-1.5 m-10"}
